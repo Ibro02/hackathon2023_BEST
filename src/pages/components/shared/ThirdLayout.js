58ee1796f87fd9fd69ffa33ee18fe54c421ce93f
Ibro02 <ibrahimhodzic2002@gmail.com>
2023-02-24 00:00:57 +0100

    v1.1.2. - Fixed logo

--- src/pages/components/shared/ThirdLayout.js
+++ src/pages/components/shared/ThirdLayout.js
@@ -3,7 +3,7 @@ import { Box } from '@chakra-ui/react'
 
 function ThirdLayout({children}) {
     return (
-        <Box alignItems={"center"}   bgColor={"whiteAlpha.700"} color={"gray.600"} boxShadow={"0px 10px 80px 10px black"} backgroundRepeat={"no-repeat"} backgroundSize={"cover"} backgroundImage={"/BlackNet3.svg"}>
+        <Box alignItems={"center"}   bgColor={"whiteAlpha.700"} color={"#292929"} boxShadow={"0px 10px 80px 10px black"} backgroundRepeat={"no-repeat"} backgroundSize={"cover"} backgroundImage={"/BlackNet3.svg"}>
         {children}
         </Box>
           )
