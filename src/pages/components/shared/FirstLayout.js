fef6f0cfa6541f8531fd8460a02b6fd853f70077
Ibro02 <ibrahimhodzic2002@gmail.com>
2023-02-24 00:23:01 +0100

    v1.1.3. - Background of firstlayout has beem changed

--- src/pages/components/shared/FirstLayout.js
+++ src/pages/components/shared/FirstLayout.js
@@ -3,7 +3,7 @@ import { Box } from '@chakra-ui/react'
 
 function FirstLayout({children}) {
   return (
-<Box align={"center"} fontSize={{base: "5xl", md:"6xl"}} py={"20"}  color={"white"} bgAttachment={"fixed"} minH={"100vh"} bgSize={'cover'} width={"100%"} backgroundImage={"/OrganizersBg.svg"}>
+<Box align={"center"} fontSize={{base: "5xl", md:"6xl"}} py={"20"}  color={"white"} bgAttachment={"scroll"} minH={"100vh"} bgSize={'cover'} width={"100%"} backgroundImage={"/Net3.svg"}>
 {children}
 </Box>
   )
